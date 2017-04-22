65117a4e34bfd0e608fd5b0a740c3a7a448e40b4
y0m0 <lixi.daniele@gmail.com>
2017-04-22 16:20:58 +0200

    print names with number before

---
+++ Step 8/ex1.rb
@@ -0,0 +1,39 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty repeat this
+  while !name.empty? do
+    # add student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  students
+end
+
+ def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+ end
+
+
+def print_students(names)
+  names.each_with_index do |student, i|
+    puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+
+students = input_students
+print_header
+print_students(students)
+print_footer(students)
