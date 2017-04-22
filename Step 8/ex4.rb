caf1f16e1e1ae167ecb1d1305c916c90c54ed447
y0m0 <lixi.daniele@gmail.com>
2017-04-22 16:39:26 +0200

    rewrote print_students using while loop

---
+++ Step 8/ex4.rb
@@ -0,0 +1,41 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty repeat this
+  while !name.empty? do
+    # add student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  students
+end
+
+ def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+ end
+
+
+def print_students(names)
+  count = 0
+  while count <= names.size - 1
+    puts "#{names[count][:name]} (#{names[count][:cohort]} cohort)"
+    count += 1
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+
+students = input_students
+print_header
+print_students(students)
+print_footer(students)
