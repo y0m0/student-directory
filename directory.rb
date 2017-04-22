ecdcaecf8676c42a1e3fb09bac21a862e20333a8
y0m0 <lixi.daniele@gmail.com>
2017-04-22 14:36:47 +0200

    defined method to print student list and count

--- directory.rb
+++ directory.rb
@@ -12,11 +12,22 @@ students = [
   "Joffrey Baratheon",
   "Norma Bates"
  ]
-# and then print them
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-  puts student
+
+ def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+ end
+
+def print(names)
+  names.each do |student|
+    puts student
+  end
 end
-# finally, we print the total number of students
-puts "Overall, we have #{students.count} great students"
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
