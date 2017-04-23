4a5391c95315b36a0844e43dbdcbca888c7b3f65
y0m0 <lixi.daniele@gmail.com>
2017-04-23 17:45:20 +0200

    imporved print formatting

---
+++ Step 8/ex6.rb
@@ -0,0 +1,57 @@
+def input_students
+  students = []
+  puts "Please enter the names of the student"
+  # get the first name
+  name = gets.chomp
+
+  # while the name is not empty repeat this
+  while !name.empty? do
+    # add student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  students
+end
+
+ def print_header
+  puts "The students of Villains Academy".center(40)
+  puts "-" * 40
+  puts "NAME".ljust(34) + "COHORT"
+ end
+
+
+def print_students(names)
+  count = 0
+  while count <= names.size - 1
+    name = names[count][:name]
+    cohort = names[count][:cohort]
+    puts "#{name}".ljust(40-cohort.size) + "#{cohort}"
+
+    count += 1
+  end
+end
+
+def print_footer(names)
+  puts "-" * 40
+  puts "Overall, we have #{names.count} great students".center(40)
+end
+
+
+
+
+students = input_students
+print_header
+print_students(students)
+print_footer(students)
+
+# =>
+
+# >> Please enter the names of the student
+
+# ~> NoMethodError
+# ~> undefined method `chomp' for nil:NilClass
+# ~>
+# ~> /Users/daniele/Git/Projects/student-directory/Step 8/ex5.rb:5:in `input_students'
+# ~> /Users/daniele/Git/Projects/student-directory/Step 8/ex5.rb:45:in `<main>'
