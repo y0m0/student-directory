9602605845919826ae4619b1615a3a5771d0fea0
y0m0 <lixi.daniele@gmail.com>
2017-04-25 00:08:22 +0200

    refactored interactive menu

--- directory.rb
+++ directory.rb
@@ -1,66 +1,77 @@
-def interactive_menu
-  students = []
-  loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    # 2. read the input and save it into a variable
-    prompt
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print_students(students)
-      print_footer(students)
-    when "9"
-      exit(0)
-    else
-      puts "I don't know what you meant, try again"
-    end
-  end
-end
+# list of all students
+@students = []
 
 def prompt
   print ">> "
+end
+
+def interactive_menu
+  loop do
+    # 1. print the menu and ask the user what to do
+    print_menu
+    # 2. read the input and save it into a variable
+    prompt
+    # 3. do what the user has asked
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def process(selection)
+  case selection
+  when '1'
+    input_students
+  when '2'
+    show_students
+  when '9'
+    exit(0)
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
 end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # create an empty array
-  students = []
   # get the first name
   prompt
   name = gets.chomp
   # while the name is not empty repeat this
   while !name.empty? do
     # add student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     # get another name from the user
+    prompt
     name = gets.chomp
   end
-  students
 end
 
- def print_header
+def print_header
   puts "The students of Villains Academy"
   puts "-------------"
- end
+end
 
-
-def print_students(names)
-  names.each do |student|
+def print_students_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 
